e5c3f16632158c970df2598ccdf73d98b583bad8
Carlos Jhonne <carlosjhonne7@gmail.com>
2025-06-11 19:06:51 -0300

    .

--- AppSmartWeb_Projeto-Base/AppSmartWeb_Projeto-Base/loja-pedido/src/main/java/com/exemplo/loja_pedido/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.loja_pedido.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exemplo.loja_pedido.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
-
