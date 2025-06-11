963c94fc60b18380be0e983252ff348b130fe01e
Carlos Jhonne <carlosjhonne7@gmail.com>
2025-06-11 20:01:37 -0300

    .

--- ControleSmartWEB/CLPmonitor1/clpmonitor/src/main/java/com/clpmonitor/clpmonitor/repository/dboBlockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clpmonitor.clpmonitor.repository;
-
-import com.clpmonitor.clpmonitor.model.dboBlock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface dboBlockRepository extends JpaRepository<dboBlock, Long>
-{
-
-}
