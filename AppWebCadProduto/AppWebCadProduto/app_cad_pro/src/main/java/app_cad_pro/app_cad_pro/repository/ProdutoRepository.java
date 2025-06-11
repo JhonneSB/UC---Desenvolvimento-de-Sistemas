7422c8e8f23deb3b5a19e7eb4ea6780d9d15df67
Carlos Jhonne <carlosjhonne7@gmail.com>
2025-06-11 19:37:36 -0300

    .

--- AppWebCadProduto/AppWebCadProduto/app_cad_pro/src/main/java/app_cad_pro/app_cad_pro/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package app_cad_pro.app_cad_pro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app_cad_pro.app_cad_pro.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
